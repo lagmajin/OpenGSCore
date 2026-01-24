859792dd8e75a87980565d20ed637daa721b64f3
lagmajin <lagmajin@gmail.com>
2026-01-24 17:50:41 +0900

    　

--- GameScene.cs
+++ GameScene.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace OpenGSCore
@@ -8,41 +9,52 @@ namespace OpenGSCore
     public class GameScene
     {
         private List<AbstractGameObject> objects = new();
+        private Dictionary<string, PlayerGameObject> players = new();
 
-        void AddBullet(BulletGameObject bullet)
+        public void AddBullet(BulletGameObject bullet)
         {
             objects.Add(bullet);
         }
 
-        void AddPlayerCharacter(PlayerGameObject character)
+        public void AddPlayerCharacter(PlayerGameObject character)
         {
-            objects.Add(character);
+            if (!players.ContainsKey(character.Id))
+            {
+                players[character.Id] = character;
+                objects.Add(character);
+            }
         }
 
-        void AddFieldItem(AbstractFieldItem item)
+        public void AddFieldItem(AbstractFieldItem item)
         {
             objects.Add(item);
         }
 
-        void AddGrenade()
+        public void AddGrenade()
         {
 
         }
 
-        void AddFlag()
+        public void AddFlag()
         {
 
 
+        }
+
+        public void RemovePlayer(string playerId)
+        {
+            if (players.TryGetValue(playerId, out var player))
+            {
+                objects.Remove(player);
+                players.Remove(playerId);
+            }
         }
 
         public void UpdateFrame()
         {
             foreach (var obj in objects)
             {
-
                 obj.Update();
-
-
             }
         }
 
@@ -51,27 +63,46 @@ namespace OpenGSCore
         public JObject ToJson()
         {
             var json = new JObject();
+            var playersJson = new JArray();
+            var bulletsJson = new JArray();
+            var itemsJson = new JArray();
 
-            foreach (var item in objects)
+            foreach (var obj in objects)
             {
-                var temp = new JObject();
+                var temp = new JObject
+                {
+                    ["Name"] = obj.Name,
+                    ["ID"] = obj.Id,
+                    ["PosX"] = obj.Posx,
+                    ["PosY"] = obj.Posy
+                };
 
-                temp["Name"] = item.Name;
-                temp["ID"] = item.Id;
-                temp["PosX"] = item.Posx;
-                temp["PosY"] = item.Posy;
-
-                json.Add(temp);
-
+                if (obj is PlayerGameObject) playersJson.Add(temp);
+                else if (obj is BulletGameObject) bulletsJson.Add(temp);
+                else if (obj is AbstractFieldItem) itemsJson.Add(temp);
             }
 
+            json["Players"] = playersJson;
+            json["Bullets"] = bulletsJson;
+            json["Items"] = itemsJson;
             return json;
         }
 
         public JObject AllPlayerDataToJson()
         {
             var json = new JObject();
-
+            foreach (var obj in objects)
+            {
+                if (obj is PlayerGameObject player)
+                {
+                    json[player.Id] = new JObject
+                    {
+                        ["Name"] = player.Name,
+                        ["PosX"] = player.Posx,
+                        ["PosY"] = player.Posy
+                    };
+                }
+            }
             return json;
         }
 
